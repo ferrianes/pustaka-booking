a1449c9a974deabc954f7c497c8e14ffdb5aa657
ferrianes <ferrian27@gmail.com>
2020-03-30 19:41:21 +0700

    uncomment modal dari home.php

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -23,14 +23,14 @@ class Home extends CI_Controller {
 
             $this->load->view('templates/templates-user/header', $data);
             $this->load->view('buku/daftarbuku', $data);
-            // $this->load->view('templates/templates-user/modal');
+            $this->load->view('templates/templates-user/modal');
             $this->load->view('templates/templates-user/footer', $data);
         } else {
             $data['user'] = 'Pengunjung';
 
             $this->load->view('templates/templates-user/header', $data);
             $this->load->view('buku/daftarbuku', $data);
-            // $this->load->view('templates/templates-user/modal');
+            $this->load->view('templates/templates-user/modal');
             $this->load->view('templates/templates-user/footer', $data);
         }
     }
