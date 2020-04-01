efb57c3bc234c92cd246525887de79856708b3ca
ferrianes <ferrian27@gmail.com>
2020-04-01 21:22:37 +0700

    add ubah-member.php

---
+++ application/views/member/ubah-member.php
@@ -0,0 +1,51 @@
+<!-- Begin Page Content -->
+<!-- Container Fluid -->
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-9">
+            <?= form_open_multipart('member/ubahprofil'); ?>
+            <div class="form-group row">
+                <label for="email" class="col-sm-2 col-form-label">Email</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="email" name="email" value="<?= $email; ?>" readonly>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="nama" class="col-sm-2 col-form-label">Nama Lengkap</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="nama" name="nama" value="<?= $user; ?>">
+                    <?= form_error('nama', '<small class="text-danger pl-2">', '</small>'); ?>
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="col-sm-2">Gambar</div>
+                <div class="col-sm-10">
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <img src="<?= base_url('assets/img/profile/') . $image; ?>" alt="Gambar Profil" class="img-thumbnail">
+                        </div>
+                        <div class="col-sm-9">
+                            <div class="custom-file">
+                                <input type="file" class="custom-file-input" id="image" name="image">
+                                <label for="image" class="custom-file-label">Pilih file</label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="form-group row justify-content-end">
+                <div class="col-sm-10">
+                    <button type="submit" class="btn btn-primary">Ubah</button>
+                    <button class="btn btn-dark" onclick="window.history.go(-1)"> Kembali</button>
+                </div>
+            </div>
+
+            </form>
+        </div>
+    </div>
+</div>
+<!-- /.Container Fluid -->
+
+</div>
+<!-- End of Main Content -->
