761c2d1e4a791b1577ebadabf4cc6c56f6cdb877
ferrianes <ferrian27@gmail.com>
2020-05-07 17:05:00 +0700

    add sidebar laporan

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -66,6 +66,36 @@
     <!-- Divider -->
     <hr class="sidebar-divider mt-3">
 
+    <!-- Heading -->
+    <div class="sidebar-heading">
+        Laporan
+    </div>
+
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item active">
+        <!-- Nav Item - Dashboard -->
+        <li class="nav-item">
+            <a href="<?= base_url('laporan/laporan_buku'); ?>" class="nav-link pb-0">
+                <i class="fa fa-fw fa-address-book"></i>
+                <span>Laporan Data Buku</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="<?= base_url('laporan/laporan_anggota'); ?>" class="nav-link pb-0">
+                <i class="fa fa-fw fa-address-book"></i>
+                <span>Laporan Data Anggota</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="<?= base_url('laporan/laporan_pinjam'); ?>" class="nav-link pb-0">
+                <i class="fa fa-fw fa-address-book"></i>
+                <span>Laporan Peminjaman</span>
+            </a>
+        </li>
+    </li>
+    <!-- Divider -->
+    <hr class="sidebar-divider mt-3">
+
     <!-- Sidebar Toogler (Sidebar) -->
     <div class="text-center d-none d-md-inline">
         <button class="rounded-circle border-0" id="sidebarToggle"></button>
