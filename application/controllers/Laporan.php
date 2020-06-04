e383c77b57a69b76c47bc56f15b8c2736201eed2
ferrianes <ferrian27@gmail.com>
2020-06-04 10:22:24 +0700

    p9

---
+++ application/controllers/Laporan.php
@@ -0,0 +1,27 @@
+<?php
+defined('BASEPATH') or exit('No Direct script access allowed');
+date_default_timezone_set('Asia/Jakarta');
+
+class Laporan extends CI_Controller {
+
+    public function laporan_buku()
+    {
+        $data['judul'] = 'Laporan Data Buku';
+        $data['user'] = $this->ModelUser->cekData(['email' => $this->session->userdata('email')])->row_array();
+        $data['buku'] = $this->ModelBuku->getBuku()->result_array();
+        $data['kategori'] = $this->ModelBuku->getKategori()->result_array();
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('buku/laporan_buku', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function cetak_laporan_buku()
+    {
+        $data['buku'] = $this->ModelBuku->getBuku()->result_array();
+        $data['kategori'] = $this->ModelBuku->getKategori()->result_Array();
+
+        $this->load->view('buku/laporan_print_buku', $data);
+    }
+}
